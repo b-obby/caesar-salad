d696c4fd895020d1ac1beab881e5c336639cb676
bobby <157441224+b-obby@users.noreply.github.com>
2024-01-23 07:01:49 +0000

    caesar brute force

--- brute.py
+++ brute.py
@@ -1,12 +1,17 @@
-message = 'this is our encrypted caesar message'
+message = 'xf mpwf dbftbs tbmbet' #encrypted text goes here
 
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+message = message.upper() 
+    # for simplicity, this script can only pass through upper case letters. 
+    #To bring less burden on the user, the script will automatically convert lower-cased submissions to upper. 
 
 for key in range(len(alphabet)):
     decryption = ''
 
     for ch in message: 
         if ch in alphabet:
+            num = alphabet.find(ch)
             num = num - key
             
             if num <0:
@@ -16,5 +21,7 @@ for key in range(len(alphabet)):
         else:
             decryption = decryption + ch
 
-    print('Brute Force Key is %s: %s'(key, decryption))
-
+    print('Attepting Key %s: %s' % (key, decryption))
+#the script will attempt every positioning option in a caesarian shuffle. 
+#the user combs through all 26 options to find out which result has their deciphered text. 
+#The key in the output represents how many positions the plaintext's alphabet was shifted. 
