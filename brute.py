9a7484d34e320ac3a1772aec0646e307070dc695
bobby <157441224+b-obby@users.noreply.github.com>
2024-01-23 01:42:20 -0500

    caesar brute force

--- brute.py
+++ brute.py
@@ -1,1 +1,20 @@
+message = 'this is our encrypted caesar message'
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(alphabet)):
+    decryption = ''
+
+    for ch in message: 
+        if ch in alphabet:
+            num = num - key
+            
+            if num <0:
+                num = num + len(alphabet)
+            decryption = decryption + alphabet[num]
+
+        else:
+            decryption = decryption + ch
+
+    print('Brute Force Key is %s: %s'(key, decryption))
 
